a9e4af414e74ae0a809784271d5ba4689740df02
Chen Chen <codingmanon@163.com>
2017-08-29 20:34:05 +0800

    【修改】完成第14章习题 -- 对继承有了一定的认知

--- NoteBook/2001-imodules/test.py
+++ NoteBook/2001-imodules/test.py
@@ -25,8 +25,8 @@ class BankAccount:                      # 类   class = BankAccount
               "monye = " + str(money)
         return msg
 
-    def balance(self):                  # 方法 查看余额
-        print("money = " + self.money)
+    def showMoney(self):                # 方法 查看余额
+        print("money = " + str(self.money))
 
     def putMoneyIn(self, money):        # 方法 存钱
         if money > 0:
@@ -45,18 +45,32 @@ class BankAccount:                      # 类   class = BankAccount
 # -----------
 # 创建一个蓝图
 class InterestAccount(BankAccount):     # 类   class = InterestAccount
-    def __init__(self):
-        BankAccount.__init__(self)      # 属性 继承BankAccount 的__init__() 方法
+    def __init__(self, name, ID, money):# 属性 继承BankAccount 的__init__() 方法
+        BankAccount.__init__(self, name, ID, money) # 必须带上self 参数，于是这个子类就有了公共类的那些属性数据
+                                                    # 创建利息账户的时候，会自动创建银行账户、并初始化这个账户
         self.interest = 0               # 属性 私有属性
 
-    def interest(self):                 # 方法 显示利息
-        print("interest = " + self.interest)
+    def showInterest(self):             # 方法 显示利息
+        print("interest = " + str(self.interest))
 
     def addInterest(self, race):        # 方法 增加利息
         self.interest = self.money * race
-        BankAccount.putMoneyIn(self.interest)
+        BankAccount.putMoneyIn(self, self.interest)
+        BankAccount.showMoney(self)     # 访问我的银行账户(__init__()方法里面已经初始化了这个银行账户)
 
-# ----------------------
+    def showMoney(self):                # 方法 显示余额
+        BankAccount.showMoney(self)
+
+# ---------------------
 # 由蓝图创建多个实例(对象)
-myAccount  = BankAccount('chenchen', 1001, 1000.00)
-myInterest = InterestAccount()
+myInterest = InterestAccount('chenchen', 1001, 1000.00)
+
+# -------------------
+# 使用属性数据、操作对象
+myInterest.showInterest()
+myInterest.addInterest(0.2)
+myInterest.showInterest()
+myInterest.showMoney()
+
+
+
