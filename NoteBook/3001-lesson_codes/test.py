0747545e674d7d685d035d793f2869878011c6f0
Chen Chen <codingmanon@163.com>
2017-09-16 16:03:03 +0800

    【增加】第22章  --  file + pickle

--- NoteBook/3001-lesson_codes/test.py
+++ NoteBook/3001-lesson_codes/test.py
@@ -10,7 +10,25 @@
 """
 # =======================================================================
 # modules section
+import pickle
 
-print('Number \tSquare \tCube')
-for i in range(1, 11):
-    print(i, '\t', i**2, '\t', i**3)
+words_txt  = 'others/words.txt'
+note_txt   = 'others/note.txt'
+
+# 可以使用任何类型的扩展名
+msg_pickle = 'others/msg.pkl'
+
+msg_list = ['Fred', 73, 'Hello there', 99.99e-20]
+
+pickle_file = open(msg_pickle, 'ab')
+pickle.dump('msg-1', pickle_file)
+pickle.dump(msg_list, pickle_file)
+pickle_file.close()
+
+pickle_file = open(msg_pickle, 'rb')
+new_list1 = pickle.load(pickle_file)
+new_list2 = pickle.load(pickle_file)
+pickle_file.close()
+
+print(new_list1)
+print(new_list2)
